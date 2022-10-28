6274df188fd9c2a09a994c6b635cb612f2e64428
ecco257 <connorjwittman@gmail.com>
2022-10-28 00:03:12 -0400

    edited transaction fee

--- stochastic_config.py
+++ stochastic_config.py
@@ -1,6 +1,6 @@
 ticker = 'META'
 interval = 'D' # less than a day means only a month of data, valid options are '1' '5' '15' '30' '60' 'D' 'W' 'M'
-transaction_fee = 0.01 # 1% transaction fee default
+transaction_fee = 0.02 # 2% transaction fee default
 starting_money = 10000 # $10,000 default
 sell_threshold = 80 # default 80
 buy_threshold = 20 # default 20
