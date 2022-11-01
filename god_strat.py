e226721fe384944879b7ea4f9695053438ec1567
sonic257218 <connorjwittman@gmail.com>
2022-11-01 18:15:14 -0400

    started working on new strat using stoch and vol

--- god_strat.py
+++ god_strat.py
@@ -50,10 +50,49 @@ def add_slow_moving_average_data(price_data):
     price_data['sma'] = ma
     return price_data
 
+# this is the 14 period simple moving average of 1-period emv (ease of movement) where 1-period emv is the high plus the low
+# divided by 2 minus the previous high plus the previous low divided by 2 all divided by the current volume divided by the
+# average daily volume divided by high minus low
+def add_ease_of_movement_data(price_data):
+    emv = []
+    for i in range(0, len(price_data['t'])):
+        if i < 14:
+            emv.append(0)
+        else:
+            emv.append(((price_data['h'][i] + price_data['l'][i])/2 - (price_data['h'][i-1] + price_data['l'][i-1])/2)/(price_data['v'][i]/(sum(price_data['v'][i-14:i])/14)/(price_data['h'][i] - price_data['l'][i])))
+    price_data['emv'] = emv
+    return price_data
+    
+def add_emv_moving_average_data(price_data):
+    ma = []
+    for i in range(0, len(price_data['t'])):
+        if i < 20:
+            ma.append(0)
+        else:
+            ma.append(sum(price_data['emv'][i-20:i])/20)
+    price_data['ema'] = ma
+    return price_data
+
+def add_volume_data(price_data):
+    # add the volume data to the price data
+    price_data['volume'] = price_data['v']
+    # add the simple moving average of the volume data to the price data
+    ma = []
+    for i in range(0, len(price_data['t'])):
+        if i < 14:
+            ma.append(0)
+        else:
+            ma.append(sum(price_data['volume'][i-14:i])/14)
+    price_data['sma_v'] = ma
+    return price_data
+
 def add_all_indicators(price_data):
     price_data = add_stochastic_data(price_data)
     price_data = add_fast_moving_average_data(price_data)
     price_data = add_slow_moving_average_data(price_data)
+    price_data = add_ease_of_movement_data(price_data)
+    price_data = add_emv_moving_average_data(price_data)
+    price_data = add_volume_data(price_data)
     return price_data
 
 def get_buy_and_sell_points_stoch(price_data, buy_thres, sell_thres):
@@ -63,12 +102,14 @@ def get_buy_and_sell_points_stoch(price_data, buy_thres, sell_thres):
     # go through the stochastic data and find the buy and sell points
     for i in range(1, len(price_data['t'])):
         
-        if price_data['slowk'][i] < buy_thres and price_data['slowk'][i] > price_data['slowd'][i] and \
-            price_data['slowk'][i-1] < price_data['slowd'][i-1]:
+        if price_data['slowk'][i-1] < buy_thres and price_data['slowk'][i] > buy_thres and price_data['slowk'][i] > \
+            price_data['slowd'][i] and price_data['slowk'][i-1] < price_data['slowd'][i-1] and price_data['emv'][i] - \
+            price_data['emv'][i-1] > 30:
             df.loc[len(df)] = [dr.unix_to_date(price_data['t'][i]),price_data['c'][i], 'buy']
 
-        elif price_data['slowk'][i] > sell_thres and price_data['slowk'][i] < price_data['slowd'][i] and \
-            price_data['slowk'][i-1] > price_data['slowd'][i-1]:
+        elif price_data['slowk'][i-1] > sell_thres and price_data['slowk'][i] < sell_thres and price_data['slowk'][i] <\
+            price_data['slowd'][i] and price_data['slowk'][i-1] > price_data['slowd'][i-1] and price_data['emv'][i-1] -\
+            price_data['emv'][i] > 30:
             df.loc[len(df)] = [dr.unix_to_date(price_data['t'][i]),price_data['c'][i], 'sell']
 
         else:
@@ -118,6 +159,35 @@ def get_optimal_buy_and_sell_points(price_data, buy_thres, sell_thres):
                 j -= 1  
             if j != 0:
                 df.loc[j, 'buy/sell/hold'] = 'sell'
+    return df
+
+# get the optimal buy and sell points based on the stochastic oscillator
+def get_optimal_buy_and_sell_points_stoch(price_data, buy_thres, sell_thres):
+    # initialize the data frame that will be used to store the time stamps to buy or sell the ticker and whether it is a 
+    # buy or sell
+    df = pd.DataFrame(columns=['time', 'price', 'buy/sell/hold'])
+    # initialize everything to hold
+    for i in range(0, len(price_data['t'])):
+        df.loc[len(df)] = [dr.unix_to_date(price_data['t'][i]),price_data['c'][i], 'hold']
+    # go through the stochastic data and find the buy and sell points
+    for i in range(1, len(df['time'])):
+        if price_data['slowk'][i] < buy_thres and price_data['slowk'][i] > price_data['slowd'][i] and \
+            price_data['slowk'][i-1] < price_data['slowd'][i-1]:
+            j = i
+            while j > 0 and (price_data['slowk'][j] > buy_thres or price_data['slowk'][j] < price_data['slowd'][j] or \
+                price_data['slowk'][j-1] < price_data['slowd'][j-1]):
+                j -= 1  
+            if j != 0:
+                df.loc[j, 'buy/sell/hold'] = 'buy'
+        elif price_data['slowk'][i] > sell_thres and price_data['slowk'][i] < price_data['slowd'][i] and \
+            price_data['slowk'][i-1] > price_data['slowd'][i-1]:
+            j = i
+            while j > 0 and (price_data['slowk'][j] < sell_thres or price_data['slowk'][j] > price_data['slowd'][j] or \
+                price_data['slowk'][j-1] > price_data['slowd'][j-1]):
+                j -= 1  
+            if j != 0:
+                df.loc[j, 'buy/sell/hold'] = 'sell'
+    return df
     return df
 
 # simulate the profitability if buying and selling over the time period
@@ -179,6 +249,22 @@ def plot_moving_average(indicator_data):
     fig.update_layout(title='Moving Average', xaxis_title='Date', yaxis_title='Price')
     st.plotly_chart(fig)
 
+# plot emv and ema over time on streamlit
+def plot_emv(indicator_data):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=[dr.unix_to_date(t) for t in indicator_data['t']], y=indicator_data['emv'], mode='lines', name='emv'))
+    fig.add_trace(go.Scatter(x=[dr.unix_to_date(t) for t in indicator_data['t']], y=indicator_data['ema'], mode='lines', name='ema'))
+    fig.update_layout(title='EMV', xaxis_title='Date', yaxis_title='EMV and EMA')
+    st.plotly_chart(fig)
+
+# plot the volume over time on streamlit and the simple moving average of the volume
+def plot_volume(indicator_data):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=[dr.unix_to_date(t) for t in indicator_data['t']], y=indicator_data['v'], mode='lines', name='volume'))
+    fig.add_trace(go.Scatter(x=[dr.unix_to_date(t) for t in indicator_data['t']], y=indicator_data['sma_v'], mode='lines', name='sma volume'))
+    fig.update_layout(title='Volume', xaxis_title='Date', yaxis_title='Volume')
+    st.plotly_chart(fig)
+
 st.header('Buy and Sell Points for Stochastic')
 plot_buy_and_sell_points(df1)
 st.header('Buy and Sell Points for Moving Average')
@@ -191,3 +277,5 @@ st.header('Profitability')
 st.write('Optimal profitability: ', simulate_buying_and_selling(df3)/sc.starting_money*100 - 100, '%')
 plot_stochastic_data(indicator_data)
 plot_moving_average(indicator_data)
+plot_emv(indicator_data)
+plot_volume(indicator_data)
