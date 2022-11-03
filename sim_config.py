29bd66084bf5085e3bd42d7640e37e29d0ca7b67
ecco257 <connorjwittman@gmail.com>
2022-11-03 02:51:00 -0400

    algo needs work

--- sim_config.py
+++ sim_config.py
@@ -1,6 +1,6 @@
 ticker = 'NVDA' # ticker to analyze
 crypto = 'BINANCE:ETHUSDT' # crypto to analyze
-interval = '1' # less than a day means only a month of data, valid options are '1' '5' '15' '30' '60' 'D' 'W' 'M'
+interval = 'D' # less than a day means only a month of data, valid options are '1' '5' '15' '30' '60' 'D' 'W' 'M'
 transaction_fee = 0.001 # 2% transaction fee default
 starting_money = 1000 # $1,000 default
 investment = 50 # $50 default
@@ -10,5 +10,5 @@ adx_range = 14 # default 14
 stoch_sell = 80 # default 80
 stoch_buy = 20 # default 20
 stoch_range = 14 # default 14
-optimize_params = True # default True
+optimize_params = True # default False
 optimization_depth = 100 # default 100
