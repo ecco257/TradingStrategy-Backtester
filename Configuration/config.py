ea470bf39d312b96d882d84c5f6609c02ce5b3eb
ecco257 <connorjwittman@gmail.com>
2023-03-27 11:47:35 -0400

    Update config to include hyperparameters

--- Configuration/config.py
+++ Configuration/config.py
@@ -7,15 +7,18 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 DELTA_TIME = 30
 TO_DATE_LESS = 0
-STOCK_TICKER = 'AMD' # ticker to backtest
+STOCK_TICKER = 'AMD' # stock ticker to backtest
 CRYPTO_PAIR = 'BINANCE:BTCUSDT' # crypto to backtest
 USE_CRYPTO = True # set to True to use crypto, set to False to use stocks
 INTERVAL = '15' # the amount of time in one candle, valid options are '1' '5' '15' '30' '60' 'D' 'W' 'M'
 TAKER_FEE = 0.001 # the fee for market orders as a decimal
 MAKER_FEE = 0.000 # the fee for limit orders as a decimal
-POSITION_LIMIT = 100 # at any given timestamp, this is the maximum long or short position you can have
-                     # for example, if POSITION_LIMIT = 100, and on this timestamp you have 50 long shares, you can only
-                     # buy 50 more shares, but you can sell 150 shares
-                     # NOTE: this is just per timestamp, so you can have pending limit orders that eventually get filled
-                     # and bring your position above this limit
-STRATEGY_NAME = 'ExampleMarketOrderStrategy' # the name of the strategy to backtest, must be in the Strategies folder and should not include the .py extension
+POSITION_LIMIT = 1 # the maximum gross position size, set to 0 or less for no limit
+STRATEGY_NAME = 'ExampleLimitOrderStrategy' # the name of the strategy to backtest, must be in the Strategies folder and should not include the .py extension
+STRATEGY_HYPERPARAMETERS = { # a dictionary of hyperparameters to pass to the strategy
+    'spread_pct': 0.01,
+}
+STRATEGY_HYPERPARAMETER_RANGES = { # a dictionary of hyperparameter ranges to be used when running a hyperparameter optimization
+    'spread_pct': (0.001, 0.1),
+}
+HYPER_OPT_TRIALS = 100 # the number of trials to run when doing a hyperparameter optimization
