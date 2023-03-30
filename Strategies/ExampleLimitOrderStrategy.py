fd7e4c5fbcef41ea1363c1b3eb56f163b0469dd0
ecco257 <connorjwittman@gmail.com>
2023-03-30 17:09:58 -0400

    Update limit order strategy to take in multiple states

--- Strategies/ExampleLimitOrderStrategy.py
+++ Strategies/ExampleLimitOrderStrategy.py
@@ -1,7 +1,7 @@
 from DataTypes import State, LimitOrder, MarketOrder
 from typing import List, Union, Tuple, Dict, Any
 import Configuration.Config as cfg
-from TradingFunctions import getMaxBuyQuantity, getMaxSellQuantity
+from TradingFunctions import getMaxBuyQuantity, getMaxSellQuantity, log
 import pandas as pd
 from ta import trend, momentum, volatility
 # ^ if you want to use other indicators, you can import them here. refer to https://technical-analysis-library-in-python.readthedocs.io/en/latest/ta.html
@@ -9,7 +9,7 @@ from ta import trend, momentum, volatility
 # this is an example limit order strategy to market make in a liquid market that oscillates around a mean
 # this strategy uses a hyperparameter called spread_pct, which controls how far away from the mean the limit orders are placed
 # NOTE: you must name your strategy function "strategy"
-def strategy(state: State, data: pd.DataFrame, params: Dict[str, Any]) -> Tuple[List[Union[LimitOrder, MarketOrder]], pd.DataFrame]:
+def strategy(states: Dict[str, State], data: pd.DataFrame, params: Dict[str, Any]) -> Tuple[List[Union[LimitOrder, MarketOrder]], pd.DataFrame]:
     # ---------------------------------------------------------------------------------------------
     # Step 1: Set up the data you want to use for your strategy
     # ---------------------------------------------------------------------------------------------
@@ -22,7 +22,7 @@ def strategy(state: State, data: pd.DataFrame, params: Dict[str, Any]) -> Tuple[
     # Step 2: Update the data you want to use for your strategy
     # ---------------------------------------------------------------------------------------------
 
-    data.loc[len(data)] = [state.close, state.close]
+    data.loc[len(data)] = [states['BINANCE:BTCUSDT'].close, states['BINANCE:BTCUSDT'].close]
 
     ema = trend.EMAIndicator(data['close_history'], 9, fillna=True).ema_indicator()
     current_ema = ema[len(ema)-1]
@@ -34,9 +34,9 @@ def strategy(state: State, data: pd.DataFrame, params: Dict[str, Any]) -> Tuple[
     orders: List[Union[LimitOrder, MarketOrder]] = []
 
     # basic ping pong strategy
-    if state.position >= 0:
-        orders.append(LimitOrder(state.timestamp, getMaxSellQuantity(state), data['ema'][len(data)-1]*(1+params['spread_pct'])))
-    elif state.position < 0:
-        orders.append(LimitOrder(state.timestamp, getMaxBuyQuantity(state), data['ema'][len(data)-1]*(1-params['spread_pct'])))
+    if states['BINANCE:BTCUSDT'].position >= 0:
+        orders.append(LimitOrder(states['BINANCE:BTCUSDT'].timestamp, getMaxSellQuantity(states['BINANCE:BTCUSDT']), data['ema'][len(data)-1]*(1+params['spread_pct'])))
+    elif states['BINANCE:BTCUSDT'].position < 0:
+        orders.append(LimitOrder(states['BINANCE:BTCUSDT'].timestamp, getMaxBuyQuantity(states['BINANCE:BTCUSDT']), data['ema'][len(data)-1]*(1-params['spread_pct'])))
 
     return orders, data
