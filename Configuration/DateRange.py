5e5cac40151b6f3c190a9ed99768667be047b777
ecco257 <connorjwittman@gmail.com>
2023-04-03 20:42:44 -0400

    Add support for ms unix format

--- Configuration/DateRange.py
+++ Configuration/DateRange.py
@@ -4,9 +4,13 @@ import time
 
 
 # initialize the to and from dates based on the delta_time specified in config.py
-TO_DATE_UNIX = int(time.time() - 60*60*24*cfg.TO_DATE_LESS)
+TO_DATE_UNIX = int((time.time() - 60*60*24*cfg.TO_DATE_LESS)//60 * 60)
 
 FROM_DATE_UNIX = TO_DATE_UNIX - int(cfg.DELTA_TIME * 24 * 60 * 60)
+
+TO_DATE_MS = TO_DATE_UNIX * 1000
+
+FROM_DATE_MS = TO_DATE_MS - int(cfg.DELTA_TIME * 24 * 60 * 60 * 1000)
 
 def unix_to_date(unix):
     return dt.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S')
@@ -15,4 +19,16 @@ def unix_to_date_time(unix):
     return dt.fromtimestamp(unix)
 
 def date_to_unix(date):
-    return int(time.mktime(dt.strptime(date, '%Y-%m-%d %H:%M:%S').timetuple()))
+    if type(date) == str:
+        return int(time.mktime(dt.strptime(date, '%Y-%m-%d %H:%M:%S').timetuple()))
+    else:
+        return int(time.mktime(date.timetuple()))
+
+def date_to_unix_ms(date):
+    return date_to_unix(date) * 1000
+
+def unix_ms_to_date(unix):
+    return dt.fromtimestamp(unix/1000).strftime('%Y-%m-%d %H:%M:%S')
+
+def unix_ms_to_date_time(unix):
+    return dt.fromtimestamp(unix/1000)
