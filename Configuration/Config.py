ecf1f402ab899543bafa2ea80bc85ea6016b8d28
ecco257 <connorjwittman@gmail.com>
2023-04-03 20:42:17 -0400

    Update config for ccxt

--- Configuration/Config.py
+++ Configuration/Config.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-from HyperOpt.OptimizeFunctions import byMinDrawdown, byProfit
+from HyperOpt.OptimizeFunctions import byMinDrawdown, byProfit, byNumTrades
 import pandas as pd
 from typing import Dict, List
 
@@ -11,30 +11,33 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 DELTA_TIME = 30
 TO_DATE_LESS = 0
-STOCK_TICKERS = ['AMD'] # stock tickers to backtest
-CRYPTO_PAIRS = ['BINANCE:BTCUSDT'] # cryptos to backtest
-USE_CRYPTO = True # set to True to use crypto, set to False to use stocks
 INTERVAL = '15' # the amount of time in one candle, valid options are '1' '5' '15' '30' '60' 'D' 'W' 'M'
+CRYPTO_INTERVAL = '30m' # the amount of time in one candle for crypto (ccxt)
 TAKER_FEE = 0.001 # the fee for market orders as a decimal
 MAKER_FEE = 0.000 # the fee for limit orders as a decimal
 POSITION_LIMITS: Dict[str, int] = { # the maximum number of positions to hold at once for each symbol
     'AMD': 10,
-    'BINANCE:BTCUSDT': 1,
+    'BTC/USDT': 1.0,
 }
 SYMBOLS_TO_BE_TRADED: List[str] = [
-    'BINANCE:BTCUSDT',
+    'BTC/USDT',
 ]
-STRATEGY_NAME = 'ExampleLimitOrderStrategy' # the name of the strategy to backtest, must be in the Strategies folder and should not include the .py extension
-STRATEGY_HYPERPARAMETERS = { # a dictionary of hyperparameters to pass to the strategy
-    'spread_pct': 0.01,
+STRATEGY_NAME = 'ExampleMarketOrderStrategy' # the name of the strategy to backtest, must be in the Strategies folder and should not include the .py extension
+STRATEGY_HYPERPARAMETERS = {
+    'rsi_lookback': 16,
+    'rsi_buy_threshold': 24,
+    'rsi_sell_threshold': 74,
 }
 STRATEGY_HYPERPARAMETER_RANGES = { # a dictionary of hyperparameter ranges to be used when running a hyperparameter optimization
-    'spread_pct': (0.001, 0.1),
+    'rsi_lookback': (1, 100),
+    'rsi_buy_threshold': (0.0, 50.0),
+    'rsi_sell_threshold': (50.0, 100.0),
 }
 HYPER_OPT_METHODS = [ # the methods used to optimize the hyperparameters. If one method is used 
     byMinDrawdown,
     byProfit,
+    byNumTrades,
 ]
 HYPER_OPT_TRIALS = 100 # the number of trials to run when doing a hyperparameter optimization
-SYMBOL_TO_OPTIMIZE = 'BINANCE:BTCUSDT' # the symbol to optimize the hyperparameters for. this is needed for things such as pair trading
+SYMBOL_TO_OPTIMIZE = 'BTC/USDT' # the symbol to optimize the hyperparameters for. this is needed for things such as pair trading
 PRICE_DATA: Dict[str, pd.DataFrame] = None # the price data to use for hyperparameter optimization so that the api is not called multiple times
