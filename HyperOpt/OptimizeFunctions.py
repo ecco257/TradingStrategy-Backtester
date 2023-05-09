6f658da11101ed9723e91db481d12fee35762f95
ecco257 <connorjwittman@gmail.com>
2023-05-09 03:28:05 -0400

    Add sharpe ratio optimize function

--- HyperOpt/OptimizeFunctions.py
+++ HyperOpt/OptimizeFunctions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 #--------------------------------------------------
 # Below are functions that can prioritize different things when hyperparameter optimizing
@@ -37,3 +38,19 @@ def byNumTrades(df: pd.DataFrame) -> float:
         if round(df.loc[i, 'position'], 2) != round(df.loc[i-1, 'position'], 2):
             num_trades += 1
     return num_trades
+
+def bySharpeRatio(df: pd.DataFrame) -> float:
+    # calculate the sharpe ratio
+    sharpe_ratio = 0
+    # get pnl returns, when there is no pct change, we consider this point to be 0
+    returns = df['pnl'].pct_change().fillna(0).replace([np.inf, -np.inf], 0).to_list()
+    close_returns = df['c'].pct_change().fillna(0).to_list()
+    returns_mean = sum(returns) / len(returns)
+    close_returns_mean = sum(close_returns) / len(close_returns)
+    returns_std = (sum([(x - returns_mean)**2 for x in returns]) / len(returns))**0.5
+    # calculate the sharpe ratio
+    try:
+        sharpe_ratio = (returns_mean - close_returns_mean) / returns_std
+    except ZeroDivisionError:
+        sharpe_ratio = -close_returns_mean
+    return sharpe_ratio
