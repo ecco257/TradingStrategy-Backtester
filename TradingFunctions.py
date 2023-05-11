a94c6fe451976cb55b744c5173b1d597852a28a0
ecco257 <connorjwittman@gmail.com>
2023-05-10 21:32:13 -0400

    Add trading functions to utilize capital instead of just positions

--- TradingFunctions.py
+++ TradingFunctions.py
@@ -3,11 +3,23 @@ from DataTypes import State
 from datetime import datetime as dt
 import os
 
-def getMaxBuyQuantity(state: State) -> float:
+def getMaxBuyQuantity(state: State) -> float: # this only obeys the position limits, not the cash limits
     return cfg.POSITION_LIMITS[state.security] - state.position if cfg.POSITION_LIMITS[state.security] > 0 else 100
 
-def getMaxSellQuantity(state: State) -> float:
+def getMaxSellQuantity(state: State) -> float: # this only obeys the position limits, not the cash limits
     return -(state.position + cfg.POSITION_LIMITS[state.security]) if cfg.POSITION_LIMITS[state.security] > 0 else -100
+
+def getMaxBuyQuantityUsingCash(state: State) -> float: # this assumes that we are only allowed to buy what we can afford with our cash
+    if state.pnl > 0:
+        return min(state.pnl / state.close, getMaxBuyQuantity(state))
+    else:
+        return 0
+
+def getMaxSellQuantityUsingPosition(state: State) -> float: # this assumes that we are only allowed to sell the positive positions we have, i.e. no shorting
+    if state.position > 0:
+        return -state.position
+    else:
+        return 0
 
 def log(msg: str, log_file_name: str):
     # write the message to the log file
