1ade8e4228e4fea7c8f4f34b6f5980336f4d7539
ecco257 <connorjwittman@gmail.com>
2023-05-25 16:46:33 -0400

    Add command line arguments to generate or plot

--- HMMTraining/GenModel.py
+++ HMMTraining/GenModel.py
@@ -3,12 +3,14 @@ import sys
 # (in terminal with current directory set to HMMTraining)
 sys.path[0] += '/..'
 import Configuration.Config as cfg
+import Configuration.DateRange as dr
 from Backtester import getDataForSymbol
 import os
 from HMMTraining.TrainingMethods import training_methods
 import numpy as np
 from hmmlearn.hmm import GaussianHMM
 import pickle
+import matplotlib.pyplot as plt
 
 def trainModel():
 
@@ -30,5 +32,33 @@ def trainModel():
     with open('Models/' + cfg.STRATEGY_NAME + '_' + cfg.SYMBOL_TO_TRAIN.replace('/', '_') + '_' + str(i) + '.pkl', 'wb') as file:
         pickle.dump(model, file)
 
+def plotModel():
+
+    with open('Models/' + str(cfg.MODEL_TO_USE) + '.pkl', 'rb') as file:
+        model = pickle.load(file)
+
+    price_data = getDataForSymbol(cfg.SYMBOL_TO_TRAIN)
+
+    training_data = [training_methods[i](price_data) for i in range(len(training_methods))]
+
+    packed_data = np.column_stack(training_data)
+
+    hidden_states = model.predict(packed_data)
+
+    fig = plt.figure(figsize=(15, 8))
+    # plot the data as a scatter plot with x being the date and y being the close price, colored by the hidden state
+    ax = fig.add_subplot(111)
+    ax.scatter([dr.unix_to_date(x) for x in price_data['t']], price_data['c'], c=hidden_states, cmap='viridis')
+    ax.set_title('Hidden States Over Time')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Close Price')
+    plt.show()
+
 if __name__ == '__main__':
-    trainModel()
+    # if the argument is train, train the model, if it is plot, plot the model
+    if sys.argv[1] == 'train':
+        trainModel()
+    elif sys.argv[1] == 'plot':
+        plotModel()
+    else:
+        raise ValueError('Argument must be either train or plot')
