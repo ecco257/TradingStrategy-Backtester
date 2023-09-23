3d5774201df68d5002922952f0038a13d56f2e7f
ecco257 <connorjwittman@gmail.com>
2023-09-23 02:09:46 -0400

    Add returnsVsBenchmark and returnsVsBenchmarkOverTime graphs

--- Graphs.py
+++ Graphs.py
@@ -34,6 +34,36 @@ def rsiWithThresholds(strat_data: pd.DataFrame, result_data: Dict[str, pd.DataFr
     fig.update_yaxes(title_text='RSI', secondary_y=False)
     return fig
 
+# state our return and the return of the security we are trading against as metrics
+def returnsVsBenchmark(strat_data: pd.DataFrame, result_data: Dict[str, pd.DataFrame]) -> go.Figure:
+    strat_return = result_data[cfg.SYMBOLS_TO_BE_TRADED[0]]['pnl'].iloc[-1] / result_data[cfg.SYMBOLS_TO_BE_TRADED[0]]['pnl'].iloc[0] - 1
+    benchmark_return = result_data[cfg.SYMBOLS_TO_BE_TRADED[0]]['c'].iloc[-1] / result_data[cfg.SYMBOLS_TO_BE_TRADED[0]]['c'].iloc[0] - 1
+    fig = go.Figure(data=[go.Bar(x=['Strategy', 'Benchmark'], y=[strat_return*100, benchmark_return*100])])
+    fig.update_layout(title_text='Return vs Buy and Hold', barmode='stack')
+    return fig
+
+# for each timestamp, plot the pct change of the close from the first timestamp
+# do this for the strategy and the asset we are trading against
+def returnsVsBenchmarkOverTime(strat_data: pd.DataFrame, result_data: Dict[str, pd.DataFrame]) -> go.Figure:
+    first_price = result_data[cfg.SYMBOLS_TO_BE_TRADED[0]]['c'].iloc[0]
+    starting_capital = result_data[cfg.SYMBOLS_TO_BE_TRADED[0]]['pnl'].iloc[0]
+    assert starting_capital != 0
+    assert 'timestamp' in strat_data
+    assert len(strat_data['timestamp']) == len(result_data[cfg.SYMBOLS_TO_BE_TRADED[0]]['c'])
+    benchmark_returns = []
+    strat_returns = []
+    for i in range(len(result_data[cfg.SYMBOLS_TO_BE_TRADED[0]]['c'])):
+        benchmark_returns.append(result_data[cfg.SYMBOLS_TO_BE_TRADED[0]]['c'].iloc[i] / first_price - 1)
+        strat_returns.append(result_data[cfg.SYMBOLS_TO_BE_TRADED[0]]['pnl'].iloc[i] / starting_capital - 1)
+
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    # make the units in terms of percent
+    fig.add_trace(go.Scatter(x=[dr.unix_to_date_time(x) for x in strat_data['timestamp']], y=[x*100 for x in benchmark_returns], name='Benchmark'), secondary_y=False)
+    fig.add_trace(go.Scatter(x=[dr.unix_to_date_time(x) for x in strat_data['timestamp']], y=[x*100 for x in strat_returns], name='Strategy'), secondary_y=False)
+    fig.update_layout(title_text='Return vs Buy and Hold Over Time')
+    fig.update_yaxes(title_text='Return', secondary_y=False)
+    return fig
+
 # plot the close data colored by the hidden state, as a scatter plot
 def closeColoredByState(strat_data: pd.DataFrame, result_data: Dict[str, pd.DataFrame]) -> go.Figure:
     states = strat_data['hidden_state'].to_list()
